b710962cc2b93740476b20335fe8c5d4688d058d
Andy Baran <andy.baran@hashicorp.com>
2026-01-06 14:19:12 -0500

    output fix

--- modules/kube1/outputs.tf
+++ modules/kube1/outputs.tf
@@ -1,7 +1,6 @@
 output "kube_namespace" {
     description = "The Kubernetes namespace created for the application."
     value = kubernetes_namespace_v1.simple_app.metadata.0.name
-    type = string
     ephemeral = false
     sensitive = false
 }
