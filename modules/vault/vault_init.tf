09a0fe7ffa909fda197c076dd91a2963b1e0a3cf
Andy Baran <andy.baran@hashicorp.com>
2026-01-12 09:46:17 -0500

    storage destroyed

--- modules/vault/vault_init.tf
+++ modules/vault/vault_init.tf
@@ -60,14 +60,14 @@ resource "kubernetes_role_binding_v1" "secret_writer" {
 
 
 # Wait for Vault pods to be ready
-resource "time_sleep" "wait_for_vault" {
-  depends_on      = [helm_release.vault_cluster]
-  create_duration = "60s"
-}
+# resource "time_sleep" "wait_for_vault" {
+#   depends_on      = [helm_release.vault_cluster]
+#   create_duration = "60s"
+# }
 
 # Use Kubernetes exec to initialize Vault
 resource "kubernetes_job_v1" "vault_init" {
-  depends_on = [time_sleep.wait_for_vault]
+  # depends_on = [time_sleep.wait_for_vault]
 
   metadata {
     name      = "vault-init"
@@ -97,7 +97,7 @@ resource "kubernetes_job_v1" "vault_init" {
             done
 
             # Check if already initialized
-            # if vault status | grep -q "Initialized.*false"; then
+            if vault status | grep -q "Initialized.*false"; then
               # Initialize Vault
               vault operator init -key-shares=5 -key-threshold=3 -format=json > /tmp/init.json
               cat /tmp/init.json
@@ -124,10 +124,10 @@ resource "kubernetes_job_v1" "vault_init" {
               vault operator unseal $UNSEAL_KEY_2
               vault operator unseal $UNSEAL_KEY_3
 
-             # echo "Vault initialized and unsealed successfully"
-           # else
-            #  echo "Vault already initialized"
-           # fi
+             echo "Vault initialized and unsealed successfully"
+           else
+             echo "Vault already initialized"
+           fi
           EOT
           ]
 
