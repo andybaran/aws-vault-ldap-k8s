884da4e94f590a41dd2745613442948820a9fce2
Andy Baran <andy.baran@hashicorp.com>
2026-01-26 17:20:23 -0500

    fix outputs

--- modules/vault_ldap_secrets/outputs.tf
+++ modules/vault_ldap_secrets/outputs.tf
@@ -1,9 +1,9 @@
 output "ldap_secrets_mount_path" {
   description = "The mount path of the LDAP secrets engine"
-  value       = vault_mount.ldap.path
+  value       =  vault_ldap_secret_backend.ad.path
 }
 
 output "ldap_secrets_mount_accessor" {
   description = "The accessor of the LDAP secrets engine mount"
-  value       = vault_mount.ldap.accessor
+  value       = vault_ldap_secret_backend.ad.accessor
 }
