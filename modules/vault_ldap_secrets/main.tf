0d38a3918b7ab1b81623043490ecae401e04957f
Andy Baran <andy.baran@hashicorp.com>
2026-01-26 17:17:27 -0500

    fix it

--- modules/vault_ldap_secrets/main.tf
+++ modules/vault_ldap_secrets/main.tf
@@ -1,20 +1,21 @@
-# Enable the LDAP secrets engine
-resource "vault_mount" "ldap" {
+# Enable and configure the LDAP secrets engine for Active Directory
+# Reference: https://registry.terraform.io/providers/hashicorp/vault/latest/docs/resources/ldap_secret_backend
+resource "vault_ldap_secret_backend" "ad" {
   path        = var.secrets_mount_path
-  type        = "ldap"
   description = "LDAP secrets engine for Active Directory"
-}
 
-# Configure the LDAP secrets engine for Active Directory
-resource "vault_ldap_secret_backend_config" "ad" {
-  mount    = vault_mount.ldap.path
-  schema   = "ad"
-  url      = var.ldap_url
+  # LDAP connection settings
   binddn   = var.ldap_binddn
   bindpass = var.ldap_bindpass
-  userdn   = var.ldap_userdn
+  url      = var.ldap_url
 
-  # Do not rotate the administrator password
-  # The rotate-root endpoint should NOT be called for this configuration
-  # since we're using the main administrator account
+  # Active Directory schema
+  schema = "ad"
+
+  # User search base DN
+  userdn = var.ldap_userdn
+
+  # Do not rotate the administrator password on initial setup
+  # Since we're using the main administrator account, we skip rotation
+  skip_static_role_import_rotation = true
 }
