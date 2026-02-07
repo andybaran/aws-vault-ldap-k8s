ca1694d08a0354521ea872756a001a01b9c67342
Andy Baran <andy.baran@hashicorp.com>
2026-02-07 13:41:38 -0500

    fix: simplify shared_internal SG to allow all VPC CIDR traffic (#91)
    Replace per-port security group rules (LDAP, LDAPS, Kerberos, DNS) that
    referenced the EKS node security group ID with a single ingress rule
    allowing all traffic from the VPC CIDR (10.0.0.0/16).
    
    Windows pods get secondary IPs via VPC CNI which may not match the node
    security group source filter, preventing the create-ad-user pod from
    reaching the domain controller. Allowing all VPC CIDR traffic is
    appropriate for this demo project.
    
    Closes #91

--- modules/kube0/2_security_groups.tf
+++ modules/kube0/2_security_groups.tf
@@ -1,14 +1,15 @@
 resource "aws_security_group" "shared_internal" {
   name        = "${local.resources_prefix}-shared-internal"
-  description = "Shared security group for internal communication between admin VM and domain controller"
+  description = "Shared security group for internal VPC communication (demo - allows all VPC traffic)"
   vpc_id      = module.vpc.vpc_id
 
+  # Allow all traffic from within the VPC (EKS pods, nodes, admin VM, DC)
   ingress {
-    description = "All traffic from members of this security group"
+    description = "All traffic from VPC CIDR"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    self        = true
+    cidr_blocks = [local.vpc_cidr]
   }
 
   egress {
@@ -22,69 +23,4 @@ resource "aws_security_group" "shared_internal" {
   tags = {
     Name = "${local.resources_prefix}-shared-internal"
   }
-}
-
-# Security group rule to allow LDAP traffic from EKS cluster to LDAP server
-# This enables the create-ad-user job running in EKS to connect to the DC
-resource "aws_security_group_rule" "ldap_from_eks" {
-  description              = "Allow LDAP traffic from EKS cluster nodes"
-  type                     = "ingress"
-  from_port                = 389
-  to_port                  = 389
-  protocol                 = "tcp"
-  source_security_group_id = module.eks.node_security_group_id
-  security_group_id        = aws_security_group.shared_internal.id
-}
-
-# Allow LDAPS (secure LDAP) as well in case it's needed
-resource "aws_security_group_rule" "ldaps_from_eks" {
-  description              = "Allow LDAPS traffic from EKS cluster nodes"
-  type                     = "ingress"
-  from_port                = 636
-  to_port                  = 636
-  protocol                 = "tcp"
-  source_security_group_id = module.eks.node_security_group_id
-  security_group_id        = aws_security_group.shared_internal.id
-}
-
-# Allow Kerberos from EKS (may be needed for AD authentication)
-resource "aws_security_group_rule" "kerberos_tcp_from_eks" {
-  description              = "Allow Kerberos TCP traffic from EKS cluster nodes"
-  type                     = "ingress"
-  from_port                = 88
-  to_port                  = 88
-  protocol                 = "tcp"
-  source_security_group_id = module.eks.node_security_group_id
-  security_group_id        = aws_security_group.shared_internal.id
-}
-
-resource "aws_security_group_rule" "kerberos_udp_from_eks" {
-  description              = "Allow Kerberos UDP traffic from EKS cluster nodes"
-  type                     = "ingress"
-  from_port                = 88
-  to_port                  = 88
-  protocol                 = "udp"
-  source_security_group_id = module.eks.node_security_group_id
-  security_group_id        = aws_security_group.shared_internal.id
-}
-
-# Allow DNS from EKS to DC (AD DNS)
-resource "aws_security_group_rule" "dns_tcp_from_eks" {
-  description              = "Allow DNS TCP traffic from EKS cluster nodes"
-  type                     = "ingress"
-  from_port                = 53
-  to_port                  = 53
-  protocol                 = "tcp"
-  source_security_group_id = module.eks.node_security_group_id
-  security_group_id        = aws_security_group.shared_internal.id
-}
-
-resource "aws_security_group_rule" "dns_udp_from_eks" {
-  description              = "Allow DNS UDP traffic from EKS cluster nodes"
-  type                     = "ingress"
-  from_port                = 53
-  to_port                  = 53
-  protocol                 = "udp"
-  source_security_group_id = module.eks.node_security_group_id
-  security_group_id        = aws_security_group.shared_internal.id
 }
